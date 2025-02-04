2d9e2faed7366e2ebe5f1b599be490bfc802e82a
ashish <ashish@letsenkindle.com>
2025-02-04 18:08:52 +0530

    live code 4-1-2025

--- header.php
+++ header.php
@@ -1,57 +1,66 @@
+<style>
+    @media (min-width: 1024px) and (max-width: 1300px) {
+        .top-header .navbar-light .navbar-nav .nav-link {
+            padding-left: 10px;
+            padding-right: 10px;
+            font-size: 14px;
+        }
+    }
+</style>
 <header class="top-header" data-aos="fade-down">
-            <div class="container">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="index.php"><img src="images/NUCHEM-LOGO.png" alt=""
-                            class="main-logo"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    About Us
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="about.php">Overview </a></li>
-                                    <li><a class="dropdown-item" href="management.php">Management</a></li>
-                                    <li><a class="dropdown-item" href="manufacture.php">Manufacture</a></li>
-                                    <li><a class="dropdown-item" href="certifications.php">Certifications</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown"> 
-                                <a class="nav-link" href="products.php">
-                                    Products
-                                </a>
-                                <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="index.php"><img src="images/NUCHEM-LOGO.png" alt=""
+                    class="main-logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            About Us
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="about.php">Overview </a></li>
+                            <li><a class="dropdown-item" href="management.php">Management</a></li>
+                            <li><a class="dropdown-item" href="manufacture.php">Manufacture</a></li>
+                            <li><a class="dropdown-item" href="certifications.php">Certifications</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="products.php">
+                            Products
+                        </a>
+                        <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="cellulosic-dyeing-series.php">Cellulosic Dye Series</a>
                                     </li>
                                     <li><a class="dropdown-item" href="cellulosic-printing-series.php">Cellulosic Printing Series</a></li>
                                     <li><a class="dropdown-item" href="dischargeable-dyes-series.php">Dischageble Dyes Series</a></li>
                                     <li><a class="dropdown-item" href="customised-solution.php">CUSTOMISED SOLUTIONS FOR DYEING</a></li>
                                 </ul> -->
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="sustainabilty.php">SUSTAINABILTY</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="qualitycontrol.php">QUALITY CONTROL</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Contact Us</a>
-                            </li>
-                        </ul>
-                        <!-- <form class="d-flex">
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sustainabilty.php">SUSTAINABILTY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="qualitycontrol.php">QUALITY CONTROL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+                <!-- <form class="d-flex">
                                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Search</button>
                             </form> -->
-                    </div>
-                </nav>
             </div>
-        </header>
+        </nav>
+    </div>
+</header>
